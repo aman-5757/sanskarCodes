9339719dafb162ccbc364b03fbea07e7f3786457
Aman Srivastava <aman4863@gmail.com>
2023-10-09 19:06:25 +0530

    added

---
+++ 03. 2D Matrix/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    //HW: https://leetcode.com/problems/transpose-matrix/
+    //HW: https://leetcode.com/problems/rotate-image
+
+    public static void main(String [] args){
+        
+    }
+}
